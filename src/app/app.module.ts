ae18d1029fc9c058740a2abe5de4a431f7438467
Carson Yan <ayokaman@gmail.com>
2018-09-11 23:58:27 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { BlackboxComponent } from './blackbox/blackbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    BlackboxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/blackbox/blackbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blackbox',
+  templateUrl: './blackbox.component.html',
+  styleUrls: ['./blackbox.component.css']
+})
+export class BlackboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
